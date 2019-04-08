eec5628889591d88157df0c09858bbd38db289db
narayanaparapalli <49363440+narayanaparapalli@users.noreply.github.com>
2019-04-08 21:34:23 +0530

    Update settings.py

--- curdpro/curdpro/settings.py
+++ curdpro/curdpro/settings.py
@@ -76,12 +76,18 @@ WSGI_APPLICATION = 'curdpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'curddb',
+#         'USER':'root',
+#         'PASSWORD':'prem@505356',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'curddb',
-        'USER':'root',
-        'PASSWORD':'prem@505356',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
